18b32e6e6038054a234a0fedefe66dffecea0b32
Eric Zinnikas <ericwz@fb.com>
2019-05-17 08:58:41 -0700

    Bump & remove solo, add debug

--- turbinia/__init__.py
+++ turbinia/__init__.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 """Main Turbinia application."""
 
-__version__ = '20181004+fb20190402'
+__version__ = '20181004+fb20190517'
 
 
 class TurbiniaException(Exception):
